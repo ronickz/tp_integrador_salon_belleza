7123e9e4688a13a1ee2f79b5fadbd0f6cd1ce602
Ezequiel Agustin Perez <perezezequiel.a04@gmail.com>
2026-06-29 22:50:28 -0300

    Delete backend_api directory

--- backend_api/src/middlewares/index.js
+++
@@ -1,3 +0,0 @@
-export { validateId, validateProduct } from "./product.middlewares.js";
-export { loggerURL } from "./logger.middlewares.js";
-export { validateSale } from "./sales.middlewares.js";
--- backend_api/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import productRoutes from "./product.routes.js";
-import salesRoutes from "./sales.routes.js";
-
-
-export {
-    productRoutes,
-    salesRoutes
-};
